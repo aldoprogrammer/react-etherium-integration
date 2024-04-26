1a8484615c407326bd80c531430173be0115b9bc
aldoprogrammer <aldobesma@gmail.com>
2024-04-26 15:51:19 +0700

    'update'

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
-
+import { useState, useEffect } from 'react';
+import { ethers } from 'ethers';
 
 function App() {
+  const [connected, setConnected] = useState(false);
+  const [id, setId] = useState(null);
+
+  const connect = async () => {
+    try {
+      if(!connected) {
+        const provider = new ethers.BrowserProvider(window.ethereum);
+        // await provider.send("eth_requestAccounts", []);
+        const signer = await provider.getSigner();
+        const address = await signer.getAddress();
+        const dispalyAddress = address?.substring(0, 6) + '...';
+        const message = "Hello world"
+        const sig = await signer.signMessage(message);
+        ethers.verifyMessage(message, sig);
+        setId(dispalyAddress);
+        setConnected(true);
+      } else {
+        window.ethereum.selectedAddress = null;
+        setConnected(false);
+      }
+    } catch (err) {
+      console.log(err.message)
+    }
+  }
   return (
     <div className="parent">
       <nav className="glass">
@@ -9,11 +34,14 @@ function App() {
         <a href="./">Contact</a>
       </nav>
       <section className="glass">
-        <button>CONNECT</button>
+        <button onClick={connect}>
+          {connected ? id : 'Connect Wallet'}
+        </button>
       </section>
       <footer className="glass">
-        @ssailsbury
-      </footer>
+  <a href="https://github.com/aldoprogrammer" target="_blank">@aldoprogrammer</a>
+</footer>
+
     </div>
   );
 }
